c703b18514528ed10456250b4b8631989adc2d6a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-09-21 23:45:33 +0100

    Rename variables

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = {
   hooks: {
     'page:before': (page) => {
-      const re = /^\s*\[snippet\]\(([^#\)]+)#?([^\)]+)?\)\s*$/gm
+      const snippetRegexp = /^\s*\[snippet\]\(([^#\)]+)#?([^\)]+)?\)\s*$/gm
 
       const dir = path.dirname(page.rawPath)
 
@@ -13,9 +13,9 @@ module.exports = {
 
       // find all [snippet]() statements,
       // read and cache target files
-      let res
-      while (res = re.exec(page.content)) {
-        const filename = res[1]
+      let snippetMatch
+      while (snippetMatch = snippetRegexp.exec(page.content)) {
+        const filename = snippetMatch[1]
         const filepath = path.join(dir, filename)
         readFilePromises.push(new Promise((resolve) => {
           fs.readFile(filepath, "utf-8", (err, source) => {
@@ -30,15 +30,16 @@ module.exports = {
       // appropriate file content
       return Promise.all(readFilePromises)
         .then(() => {
-          page.content = page.content.replace(re, function (match, filename, fragment) {
+          page.content = page.content.replace(snippetRegexp, (snippetLink, filename, fragment) => {
             const filepath = path.join(dir, filename)
             const source = sourceByFilepath[filepath]
-            if (!source) return `${match} *FILE NOT FOUND: ${filename}*`
+            if (!source) return `${snippetLink} *FILE NOT FOUND: ${filename}*`
             if (!fragment) return source
+            // find the content between two anchors, which can be "### [fragment-nane]" or "/// [fragment-nane]"
             const fragmentRegexp = new RegExp(`[\\s\\S]*(?:###|\\/\\/\\/)\\s*\\[${fragment}\\]([\\s\\S]*)(?:###|\\/\\/\\/)\\s*\\[${fragment}\\]`)
-            const m = source.match(fragmentRegexp)
-            if (!m) return `${match} *FRAGMENT NOT FOUND: ${filename}#${fragment}*` //throw new Error(`Fragment [${fragment}] not found in ${filepath}`)
-            return unindent(m[1])
+            const fragmentMatch = source.match(fragmentRegexp)
+            if (!fragmentMatch) return `${snippetLink} *FRAGMENT NOT FOUND: ${filename}#${fragment}*`
+            return unindent(fragmentMatch[1])
           })
           return page
         })
